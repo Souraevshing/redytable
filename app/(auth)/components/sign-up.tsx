079c1564c9c4d6f9a6ff65536c556efbb87196b9
Souraevshing <isauravshing@gmail.com>
2024-11-17 09:59:23 +0530

    add: initial commit

---
+++ app/(auth)/components/sign-up.tsx
@@ -0,0 +1,5 @@
+export const SignUp = () => {
+  return <div>sign-up</div>;
+};
+
+export default SignUp;
---
+++ app/(auth)/components/verify-otp.tsx
@@ -0,0 +1,5 @@
+export const VerifyOTP = () => {
+  return <div>Verify otp</div>;
+};
+
+export default VerifyOTP;
