311e92fb0074997d17438647fcdc1650cc7d8573
Souraevshing <isauravshing@gmail.com>
2024-11-23 10:56:23 +0530

    remove: remove not-found page

--- app/_not-found/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function PageNotFound() {
-  return (
-    <div className="flex items-center text-center font-extrabold text-red-400">
-      404 - page not found
-    </div>
-  );
-}
