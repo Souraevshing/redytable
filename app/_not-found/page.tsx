9c3c756b69e891eface9653abd90000ced9ad0b0
Souraevshing <isauravshing@gmail.com>
2024-11-23 10:45:38 +0530

    feat: add not-found page

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="flex items-center text-center font-extrabold text-red-400">
+      404 - page not found
+    </div>
+  );
+}
